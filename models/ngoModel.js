17861364e3138c66e84c282eb41f3c7df8696108
AyaNS2003 <aya.sawalha.2003@gmail.com>
2025-11-08 21:29:28 +0200

    adding new NGO

---
+++ models/ngoModel.js
@@ -0,0 +1,15 @@
+const db = require('../config/db')
+const { v4: uuidv4 } = require("uuid")
+
+class NGO {
+    static async add(data) {
+        const { userId, organizationName, registrationNumber, contactPerson, serviceAreas } = data;
+        const ngoId = uuidv4()
+
+        //here we should check for user id if exist or not 
+        const qry = "INSERT INTO healthpal.ngo (userId,organizationName,registrationNumber,contactPerson,serviceAreas)VALUES(?, ?, ?, ?, ?);"
+        const res = await db.execute(qry, [userId, organizationName, registrationNumber, contactPerson, serviceAreas])
+
+        return {ngoId, ...data}
+    }
+}

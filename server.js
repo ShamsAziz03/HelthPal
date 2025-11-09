a53ededc617680f0bee9552c99d623ed8aa86b3f
YaraDaraghmeh <yaradaraghmeh056@gmail.com>
2025-11-09 21:30:34 +0200

    Connecting the server with the routes

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import equipmentRouter from './routes/equipmentRoutes.js';
 dotenv.config();
 const app = express();
 
 app.use(express.json());
-
+app.use("/equipment", equipmentRouter);
 
 app.get("/", (req, res) => res.send("project API is running"));
 

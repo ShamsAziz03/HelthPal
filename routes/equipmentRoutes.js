6cdf8b83a224b8dc1f29fc20908bee6e76de5e61
YaraDaraghmeh <yaradaraghmeh056@gmail.com>
2025-11-09 21:30:01 +0200

    Adding the correct routes for the equipmant

---
+++ routes/equipmentRoutes.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const router = express.Router();
+const equipmentController = require('../controllers/equipmentController');
+
+// Equipment Routes
+router.post('/create', equipmentController.createEquipment);
+router.get('/getAll', equipmentController.getAllEquipment);
+router.get('/search', equipmentController.searchEquipment);
+router.get('/getEquipment/:equipmentId', equipmentController.getEquipmentById);
+router.get('/:ngoId', equipmentController.getEquipmentByNGO);
+router.get('/:ngoId/:status', equipmentController.getEquipmentByNGOANDStatus);
+router.get('/getStatics', equipmentController.getEquipmentStatics);
+router.get('/getExpiringEquipment', equipmentController.getExpiringEquipment);
+
+
+router.put('/updateEquipmentStatus/:equipmentId', equipmentController.updateEquipmentStatus);
+router.put('/updateEquipment/:equipmentId', equipmentController.updateEquipment);
+
+router.delete('/deleteEquipment/:equipmentId', equipmentController.deleteEquipment);
+
+module.exports = router;

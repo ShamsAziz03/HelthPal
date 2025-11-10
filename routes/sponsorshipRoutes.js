e75bdcdc16a3c2b85dfcbad3ecd4dc2b94bac18a
Raheeq Qassrawi <qassrawiraheeq@gmail.com>
2025-11-10 19:07:05 +0200

    feat(2.1): add route for getting sponsorship by id

--- routes/sponsorshipRoutes.js
+++ routes/sponsorshipRoutes.js
@@ -2,7 +2,11 @@ const express = require("express");
 const router = express.Router();
 const {
   createSponsorship,
+  getSponsorshipById,
 } = require("../controllers/sponsorshipController");
+
+// Route for getting a single sponsorship by ID
+router.get("/:sponsorshipId", getSponsorshipById);
 
 // Route for creating a new sponsorship
 router.post("/", createSponsorship);

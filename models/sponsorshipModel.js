733a55f4b29915793cb0a4367d5c0f8e36fdc0d3
Raheeq Qassrawi <qassrawiraheeq@gmail.com>
2025-11-10 18:59:30 +0200

    feat(2.1): add uuid for sponsorship id

--- models/sponsorshipModel.js
+++ models/sponsorshipModel.js
@@ -1,10 +1,10 @@
 const db = require("../config/db");
+const { v4: uuidv4 } = require("uuid");
 
 class Sponsorship {
   // Create a new sponsorship record
   static async create(sponsorshipData) {
     const {
-      sponsorshipId,
       patientId,
       donorId,
       treatmentType,
@@ -13,8 +13,9 @@ class Sponsorship {
       status,
       createdAt,
     } = sponsorshipData;
+    const sponsorshipId = uuidv4();
 
-    const query = `INSERT INTO sponsorships (sponsorshipId, patientId, donorId, treatmentType, goalAmount, currentAmount, status, createdAt)
+    const query = `INSERT INTO sponsorship (sponsorshipId, patientId, donorId, treatmentType, goalAmount, currentAmount, status, createdAt)
                     VALUES (?, ?, ?, ?, ?, ?, ?, ?)`;
     const values = [
       sponsorshipId,
@@ -32,14 +33,14 @@ class Sponsorship {
 
   // get all sponsorships
   static async findAll() {
-    const query = "SELECT * FROM sponsorships";
+    const query = "SELECT * FROM sponsorship";
     const [rows] = await db.execute(query);
     return rows;
   }
 
   // Find a sponsorship by its ID
   static async findById(sponsorshipId) {
-    const query = "SELECT * FROM sponsorships WHERE sponsorshipId = ?";
+    const query = "SELECT * FROM sponsorship WHERE sponsorshipId = ?";
     const [rows] = await db.execute(query, [sponsorshipId]);
     return rows[0];
   }
@@ -48,7 +49,7 @@ class Sponsorship {
   static async update(sponsorshipId, data) {
     const { currentAmount, status } = data;
     const query =
-      "UPDATE sponsorships SET currentAmount = ?, status = ? WHERE sponsorshipId = ?";
+      "UPDATE sponsorship SET currentAmount = ?, status = ? WHERE sponsorshipId = ?";
     const [result] = await db.execute(query, [
       currentAmount,
       status,
@@ -59,7 +60,7 @@ class Sponsorship {
 
   // Delete a sponsorship record
   static async delete(sponsorshipId) {
-    const query = "DELETE FROM sponsorships WHERE sponsorshipId = ?";
+    const query = "DELETE FROM sponsorship WHERE sponsorshipId = ?";
     const [result] = await db.execute(query, [sponsorshipId]);
     return result;
   }

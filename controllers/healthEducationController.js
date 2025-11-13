e4969b9e2e60943712273b61f2937a7efe1b5a7e
YaraDaraghmeh <yaradaraghmeh056@gmail.com>
2025-11-13 23:56:13 +0200

    Adding the create controlling function

--- controllers/healthEducationController.js
+++ controllers/healthEducationController.js
@@ -2,5 +2,19 @@ const HealthEducation = require("../models/HealthEducation");
 
 class HealthEducationController {
 
+    static async create(req, res) {
+        try {
+            const content = await HealthEducation.create(req.body);
+            res.status(201).json({
+                message: "Health education content created successfully",
+                data: content
+            });
+        } catch (error) {
+            res.status(500).json({
+                message: "Failed to create content",
+                error: error.message
+            });
+        }
+    }
 }
 

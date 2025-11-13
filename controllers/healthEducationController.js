e141ea095b2763da8a6104046e16c2f92b41fecd
YaraDaraghmeh <yaradaraghmeh056@gmail.com>
2025-11-13 23:59:53 +0200

    Get Posts Categories

--- controllers/healthEducationController.js
+++ controllers/healthEducationController.js
@@ -98,6 +98,18 @@ class HealthEducationController {
             });
         }
     }
+    static async getCategories(req, res) {
+        try {
+            const categories = await HealthEducation.getCategories();
+            res.status(200).json(categories);
+        } catch (error) {
+            res.status(500).json({
+                message: "Failed to fetch categories",
+                error: error.message
+            });
+        }
+    }
+
 
 }
 

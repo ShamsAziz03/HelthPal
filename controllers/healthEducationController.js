843689ef2f1883176c94b27c970493eac6616c9b
YaraDaraghmeh <yaradaraghmeh056@gmail.com>
2025-11-13 23:57:18 +0200

    Get Content by its ID

--- controllers/healthEducationController.js
+++ controllers/healthEducationController.js
@@ -29,5 +29,20 @@ class HealthEducationController {
             });
         }
     }
+    static async getById(req, res) {
+        try {
+            const { id } = req.params;
+            const content = await HealthEducation.getById(id);
+            if (!content) {
+                return res.status(404).json({ message: "Content not found" });
+            }
+            res.status(200).json(content);
+        } catch (error) {
+            res.status(500).json({
+                message: "Failed to fetch content",
+                error: error.message
+            });
+        }
+    }
 }
 

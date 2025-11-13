b9100b95cc5a590274233fbf5c406b3524cdb4b5
YaraDaraghmeh <yaradaraghmeh056@gmail.com>
2025-11-13 17:39:11 +0200

    Adding the get routes

--- routes/publicHealthAlertRoutes.js
+++ routes/publicHealthAlertRoutes.js
@@ -8,6 +8,8 @@ router.get('/', alertController.getAllAlerts);
 router.get('/active', alertController.getActiveAlerts);
 router.get('/recent', alertController.getRecentAlerts);
 router.get('/stats', alertController.getAlertStatistics);
-
+router.get('/severity/:severity', alertController.getAlertsBySeverity);
+router.get('/area/:area', alertController.getAlertsByArea);
+router.get('/:alertId', alertController.getAlertById);
 
 module.exports = router;

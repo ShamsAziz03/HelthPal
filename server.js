231034067fcd9ab185acc5562020bd781693521f
AyaNS2003 <aya.sawalha.2003@gmail.com>
2025-11-13 22:53:28 +0200

    add server connection

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
-import express from "express";
-import dotenv from "dotenv";
-
+const express = require("express");
+const dotenv = require("dotenv");
+const ngoRoutes = require('./routes/ngoRoutes');
+const collaborationRoutes = require('./routes/collaborationRoutes');
+const availabilityRoutes = require('./routes/drAvailabilityRoutes')
+const bookingRoutes = require('./routes/bookingRoutes')
+const missionRoutes = require('./routes/missionRoutes')
 dotenv.config();
 const app = express();
 
 app.use(express.json());
-
+app.use('/api/ngos', ngoRoutes);
+app.use('/api/collaborations', collaborationRoutes);
+app.use('/api/drAvailability', availabilityRoutes);
+app.use('/api/booking', bookingRoutes)
+app.use('/api/missions', missionRoutes)
 
 app.get("/", (req, res) => res.send("project API is running"));
 

2a9b43d8453bf611a41bbf8190df55242016457b
Raheeq Qassrawi <qassrawiraheeq@gmail.com>
2025-11-16 17:15:04 +0200

    edit conflicts to merge easily

--- config/db.js
+++ config/db.js
@@ -1,17 +1,16 @@
-const mysql = require("mysql2");
+// CommonJS everywhere
+const mysql = require("mysql2/promise");
 require("dotenv").config();
 
-const pool = mysql.createPool({
-  host: process.env.DB_HOST,
+const db = mysql.createPool({
+  host: process.env.DB_HOST || "127.0.0.1",
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  port: process.env.DB_PORT,
+  port: Number(process.env.DB_PORT) || 3306,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
 });
 
-const promisePool = pool.promise();
-
-module.exports = promisePool;
+module.exports = db;

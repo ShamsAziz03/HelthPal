b98b1b5640cc343372e8ad4439a2f7dfc5dc6c80
Raheeq Qassrawi <qassrawiraheeq@gmail.com>
2025-12-01 02:21:52 +0200

    add get all users and delete specific user

--- controllers/userController.js
+++ controllers/userController.js
@@ -57,3 +57,31 @@ exports.updateIsAnonymous = async (req, res) => {
     });
   }
 };
+
+// Get all users
+exports.getAllUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.status(200).json({ success: true, count: users.length, data: users });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error fetching users", error: error.message });
+  }
+};
+
+// Delete a user by ID
+exports.deleteUser = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const result = await User.deleteById(userId);
+    if (result.affectedRows === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    res.status(200).json({ message: "User deleted successfully" });
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "Error deleting user", error: error.message });
+  }
+};

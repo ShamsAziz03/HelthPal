db0484dc63949a7dbeb492d2b3ac0920472b0bfa
Raheeq Qassrawi <qassrawiraheeq@gmail.com>
2025-12-05 23:05:11 +0200

    add authentication using JWT in patient feedback

--- routes/medicalSponsorshipSystem/patientFeedbackRoutes.js
+++ routes/medicalSponsorshipSystem/patientFeedbackRoutes.js
@@ -6,21 +6,28 @@ const {
   getAllFeedbacks,
   getFeedbackById,
   deleteFeedbackById,
-} = require("../../controllers/MedicalSponsorshipSystem/patientFeedbackController");
+} = require("../../controllers/medicalSponsorshipSystem/patientFeedbackController");
+const {
+  authenticateToken,
+  authorizeRole,
+} = require("../../middleware/authMiddleware");
+
+// protect feedback routes
+router.use(authenticateToken);
 
 // Route for getting all patient feedbacks
-router.get("/", getAllFeedbacks);
+router.get("/", authorizeRole("Admin"), getAllFeedbacks);
 
 // Route for getting a single patient feedback by ID
 router.get("/:feedbackId", getFeedbackById);
 
 // Route for creating a new patient feedback
-router.post("/", createPatientFeedback);
+router.post("/", authorizeRole("Patient"), createPatientFeedback);
 
 // Route for getting patient feedback by sponsorship ID
 router.get("/sponsorship/:sponsorshipId", getFeedbacksBySponsorshipId);
 
 // Route for deleting a patient feedback by ID
-router.delete("/:feedbackId", deleteFeedbackById);
+router.delete("/:feedbackId", authorizeRole("Admin"), deleteFeedbackById);
 
 module.exports = router;

c7f49faad474f4821e3453f8f9ea59cbaef3c55f
ShamsAziz03 <shamsimad550@gmail.com>
2025-12-05 23:43:30 +0200

    add JWT for first feature- remote medical consultations

--- routes/remoteMedicalConsultations/sessionRoutes.js
+++ routes/remoteMedicalConsultations/sessionRoutes.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const router = express.Router();
 const SessionController = require("../../controllers/remoteMedicalConsultations/sessionController");
+const {
+  authenticateToken,
+  authorizeRole,
+} = require("../../middleware/authMiddleware");
 
-router.get("/createRoom", SessionController.createRoom);
-router.get("/getSessionInfo/:sessionId", SessionController.getSessionInfo);
-router.delete("/deleteSession/:sessionId", SessionController.deleteSession);
+router.get(
+  "/createRoom",
+  authenticateToken,
+  authorizeRole("Admin", "Doctor"),
+  SessionController.createRoom
+);
+router.get(
+  "/getSessionInfo/:sessionId",
+  authenticateToken,
+  authorizeRole("Admin", "Patient", "Doctor"),
+  SessionController.getSessionInfo
+);
+router.delete(
+  "/deleteSession/:sessionId",
+  authenticateToken,
+  authorizeRole("Admin", "Doctor"),
+  SessionController.deleteSession
+);
 
 module.exports = router;

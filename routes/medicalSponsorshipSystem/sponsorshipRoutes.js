3090a241b088b107e53814b3829e9750e5aec9fd
Raheeq Qassrawi <qassrawiraheeq@gmail.com>
2025-12-05 23:06:02 +0200

    add authentication using JWT in sponsorship

--- routes/medicalSponsorshipSystem/sponsorshipRoutes.js
+++ routes/medicalSponsorshipSystem/sponsorshipRoutes.js
@@ -10,7 +10,14 @@ const {
   updateSponsorshipProgress,
   checkSponsorshipGoal,
   deleteSponsorship,
-} = require("../../controllers/MedicalSponsorshipSystem/sponsorshipController");
+} = require("../../controllers/medicalSponsorshipSystem/sponsorshipController");
+const {
+  authenticateToken,
+  authorizeRole,
+} = require("../../middleware/authMiddleware");
+
+// apply authentication to all routes in this router
+router.use(authenticateToken);
 
 // Route for getting all sponsorships
 router.get("/", getAllSponsorships);
@@ -22,18 +29,26 @@ router.get("/:sponsorshipId", getSponsorshipById);
 router.get("/patient/:patientId", getSponsorshipsByPatientId);
 
 // Route for creating a new sponsorship
-router.post("/", createSponsorship);
+router.post("/", authorizeRole("Admin"), createSponsorship);
 
 // Route for updating a sponsorship
-router.put("/:sponsorshipId", updateSponsorship);
+router.put("/:sponsorshipId", authorizeRole("Admin"), updateSponsorship);
 
 // Route for updating sponsorship progress
-router.put("/:sponsorshipId/progress", updateSponsorshipProgress);
+router.put(
+  "/:sponsorshipId/progress",
+  authorizeRole("Admin"),
+  updateSponsorshipProgress
+);
 
 // Route for checking sponsorship goal
-router.get("/:sponsorshipId/goal", checkSponsorshipGoal);
+router.get(
+  "/:sponsorshipId/goal",
+  authorizeRole("Admin", "Donor"),
+  checkSponsorshipGoal
+);
 
 // Route for deleting a sponsorship
-router.delete("/:sponsorshipId", deleteSponsorship);
+router.delete("/:sponsorshipId", authorizeRole("Admin"), deleteSponsorship);
 
 module.exports = router;

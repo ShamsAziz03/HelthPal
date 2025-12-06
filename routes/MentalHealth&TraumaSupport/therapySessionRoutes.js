9a013221f05804c5cfad83e5f0e4e6049565af44
Raheeq Qassrawi <qassrawiraheeq@gmail.com>
2025-12-06 15:44:05 +0200

    add JWT for therapy sessions

--- routes/MentalHealth&TraumaSupport/therapySessionRoutes.js
+++ routes/MentalHealth&TraumaSupport/therapySessionRoutes.js
@@ -9,26 +9,53 @@ const {
   getTherapySessionsByPatientId,
   deleteTherapySession,
 } = require("../../controllers/MentalHealth&TraumaSupport/therapySessionController");
+const {
+  authenticateToken,
+  authorizeRole,
+} = require("../../middleware/authMiddleware");
+
+// apply authentication to all routes in this router
+router.use(authenticateToken);
 
 // Route for creating a new therapy session
-router.post("/", createTherapySession);
+router.post("/", authorizeRole("Admin", "Doctor"), createTherapySession);
 
 // Route for getting all therapy sessions
-router.get("/", getAllTherapySessions);
+router.get("/", authorizeRole("Admin"), getAllTherapySessions);
 
 // Route for getting a single therapy session by ID
-router.get("/:sessionId", getTherapySessionById);
+router.get(
+  "/:sessionId",
+  authorizeRole("Admin", "Doctor", "Patient"),
+  getTherapySessionById
+);
 
 // Route for updating a therapy session by ID
-router.put("/:sessionId", updateTherapySession);
+router.put(
+  "/:sessionId",
+  authorizeRole("Admin", "Doctor"),
+  updateTherapySession
+);
 
 // Route for getting therapy sessions by therapist ID
-router.get("/therapist/:therapistId", getTherapySessionsByTherapistId);
+router.get(
+  "/therapist/:therapistId",
+  authorizeRole("Admin", "Doctor"),
+  getTherapySessionsByTherapistId
+);
 
 // Route for getting therapy sessions by patient ID
-router.get("/patient/:patientId", getTherapySessionsByPatientId);
+router.get(
+  "/patient/:patientId",
+  authorizeRole("Admin", "Patient", "Doctor"),
+  getTherapySessionsByPatientId
+);
 
 // Route for deleting a therapy session by ID
-router.delete("/:sessionId", deleteTherapySession);
+router.delete(
+  "/:sessionId",
+  authorizeRole("Admin", "Doctor"),
+  deleteTherapySession
+);
 
 module.exports = router;
